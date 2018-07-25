27c4f757b52f60fd38ccbde48017a69dfd657dd1
vicgarfield <yue901228@qq.com>
2018-07-26 01:00:24 +0800

    add funtions add_tail and delete_node in linked_list

--- algorithm_exercise/linked_list.cc
+++ algorithm_exercise/linked_list.cc
@@ -12,7 +12,6 @@ struct ListNode {
 };
 
 void add_to_tail(ListNode** p_header, int value) { // Why is **???
-
     ListNode* p_new_node = new ListNode();
     p_new_node->m_nValue = value;
     p_new_node->m_pNext = nullptr;
@@ -26,6 +25,35 @@ void add_to_tail(ListNode** p_header, int value) { // Why is **???
             p_hold = p_hold->m_pNext;
         }
         p_hold->m_pNext = p_new_node;
+    }
+}
+
+// Delete first note whose value is designated.
+void delete_chosen_node(ListNode** p_header, int value) {
+    if (p_header == nullptr || *p_header == nullptr) {
+        std::cout << "[error] function delete_chosen_node parameter p_header is nullptr" << std::endl;
+        exit(1);
+    }
+
+    ListNode* p_tobedeleted = nullptr;
+    if ((*p_header)->m_nValue == value) {
+        p_tobedeleted = *p_header;
+        *p_header = (*p_header)->m_pNext;
+    }
+    else {
+        ListNode* p_holder = *p_header;
+        while(p_holder->m_pNext != nullptr && p_holder->m_pNext->m_nValue != value) {
+            p_holder = p_holder->m_pNext;
+        }
+        if (p_holder->m_pNext != nullptr && p_holder->m_pNext->m_nValue == value) {
+            p_tobedeleted = p_holder->m_pNext;
+            p_holder->m_pNext = p_holder->m_pNext->m_pNext;
+        }
+    }
+
+    if (p_tobedeleted != nullptr) {
+        delete p_tobedeleted;
+        p_tobedeleted = nullptr;
     }
 }
 
@@ -43,13 +71,22 @@ int main(int argc, char* argv[]) {
 
     add_to_tail(&listnode1, 3);
 
-    ListNode* p_hold;
-    p_hold = listnode1;
+    ListNode* p_hold1;
+    p_hold1 = listnode1;
 
-    while(p_hold->m_pNext != nullptr) {
-        p_hold = p_hold->m_pNext;
+    while(p_hold1->m_pNext != nullptr) {
+        p_hold1 = p_hold1->m_pNext;
     }
-    std::cout << "Tail number is " << p_hold->m_nValue << std::endl;
+    std::cout << "Tail number is " << p_hold1->m_nValue << std::endl;
+
+    delete_chosen_node(&listnode1, 2);
+
+    ListNode* p_hold2;
+    p_hold2 = listnode1;
+    while(p_hold2 != nullptr) {
+        std::cout << "node value: " << p_hold2->m_nValue << std::endl;
+        p_hold2 = p_hold2->m_pNext;
+    }
 
     std::cout << "Done!" << std::endl;
 }
